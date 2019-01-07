1dd6ca83e44e54ab5c3751889a8dac84b512cd14
Tim Broughton <TimBroughton@quickenloans.com>
2019-01-07 08:54:06 -0500

    initial commit

---
+++ CarDealershipAPI/App_Start/WebApiConfig.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace CarDealershipAPI
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+            var jsonSerializerSettings = config.Formatters.JsonFormatter.SerializerSettings;
+
+            jsonSerializerSettings.ReferenceLoopHandling =
+                Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+
+            //we just want json
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{action}/{id}",//added action because there are multiple overloads of the same types
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+    
+}
---
+++ CarDealershipAPI/Controllers/CarController.cs
@@ -0,0 +1,83 @@
+﻿using CarDealershipAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CarDealershipAPI.Controllers
+{
+    public class CarController : ApiController
+    {
+
+        //grab all the beans GET
+        [HttpGet]
+        public List<Car> GetCars()
+        {
+
+            CarsEntities ORM = new CarsEntities();
+
+            return ORM.Cars.ToList();
+
+
+        }
+
+
+        //grab one of the beans GET
+        [HttpGet]
+        public Car GetCar(int id)
+        {
+            CarsEntities ORM = new CarsEntities();
+
+            return ORM.Cars.Find(id);
+
+        }
+
+
+        //edit a bean PUT
+        [HttpPut]
+        public void EditCar(Car editCar)
+        {
+            CarsEntities ORM = new CarsEntities();
+
+            Car found = ORM.Cars.Find(editCar.CarID);
+
+            found.Color = editCar.Color;
+            found.Make = editCar.Model;
+            found.Model = editCar.Model;
+
+            ORM.Entry(found).State = System.Data.Entity.EntityState.Modified; //let them know it changed
+            ORM.SaveChanges();
+        }
+
+
+        //make a new bean POST
+        [HttpPost]
+        public void AddCar(Car newCar)
+        {
+            CarsEntities ORM = new CarsEntities();
+
+            if (ModelState.IsValid)
+            {
+                ORM.Cars.Add(newCar);
+            }
+
+
+        }
+
+
+        //delete a bean DELETE
+        [HttpDelete]
+        public void DeleteCar(int ID)
+        {
+            CarsEntities ORM = new CarsEntities();
+
+            Car found = ORM.Cars.Find(ID);
+            ORM.Cars.Remove(found);
+            ORM.SaveChanges();
+
+        }
+
+    }
+}
